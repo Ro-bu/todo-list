f2ceb6d78bbe4ea5cd857fdf2b4dfb203d765ce4
Ro-bu <robin.raid1@gmail.com>
2022-04-12 17:14:30 +0300

    added basic UI

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// title
+// description
+// date
+// project
+// subproject?
+
+const taskFactory = (title, description, date, project, subproject, color) => {
+    return {title, description, date, project, subproject, color};
+};
