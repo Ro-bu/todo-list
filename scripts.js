b3b774dd09f2806e64578213a7838fc0ba35ee08
Ro-bu <robin.raid1@gmail.com>
2022-04-13 15:11:41 +0300

    UI update

--- scripts.js
+++ scripts.js
@@ -2,8 +2,22 @@
 // description
 // date
 // project
-// subproject?
+// color
 
 const taskFactory = (title, description, date, project, subproject, color) => {
     return {title, description, date, project, subproject, color};
 };
+const projectFactory = (name, color) => {
+    return{name, color};
+};
+const projectContainer = document.querySelector(".project-container");
+document.querySelector(".sidebar-top ul li:last-child").addEventListener("click", () => {
+    // document.querySelector(".project-container").classList.toggle("invisible");
+    document.querySelector(".sidebar-top ul li:last-child").classList.toggle("rotate-arrow");
+    if(projectContainer.style.maxHeight){
+        projectContainer.style.maxHeight = null;
+
+    } else {
+        projectContainer.style.maxHeight = projectContainer.scrollHeight +"px";
+    }
+});
