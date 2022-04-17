0ef2616e25671fe46afbb3992185faeb737cd484
Ro-bu <robin.raid1@gmail.com>
2022-04-17 14:39:52 +0300

    open and clsoe the modal through UI

--- scripts.js
+++ scripts.js
@@ -1,12 +1,45 @@
-const taskFactory = (title, description, date, project, color, checked) => {
-    return {title, description, date, project, color, checked};
+const taskModal = document.querySelector("#task-modal");
+function closeTaskModal() {
+    taskModal.style.display = "none";
+};
+function openTaskModal() {
+    taskModal.style.display = "block";
+};
+document.querySelector("#task-modal-close").addEventListener("click", () =>{
+    closeTaskModal();
+});
+document.querySelector(".add-task").addEventListener("click", () =>{
+    openTaskModal();
+});
+const projectModal = document.querySelector("#project-modal");
+function closeProjectModal(){
+    projectModal.style.display = "none";
+};
+function openProjectModal(){
+    projectModal.style.display = "block";
+};
+document.querySelector("#project-modal-close").addEventListener("click", () =>{
+    closeProjectModal();
+});
+document.querySelector(".add-project").addEventListener("click", () =>{
+    openProjectModal();
+});
+window.addEventListener("click", () =>{
+    if(event.target == taskModal){
+        closeTaskModal();
+    }else if(event.target == projectModal){
+        closeProjectModal();
+    }
+});
+
+const taskFactory = (title, dueDate, project, done) => {
+    return {title, dueDate, project, done};
 };
 const projectFactory = (name, color) => {
     return{name, color};
 };
 const projectContainer = document.querySelector(".project-container");
 document.querySelector(".sidebar-top ul li:last-child").addEventListener("click", () => {
-    // document.querySelector(".project-container").classList.toggle("invisible");
     document.querySelector(".sidebar-top ul li:last-child").classList.toggle("rotate-arrow");
     if(projectContainer.style.maxHeight){
         projectContainer.style.maxHeight = null;
@@ -15,3 +48,11 @@ document.querySelector(".sidebar-top ul li:last-child").addEventListener("click"
         projectContainer.style.maxHeight = projectContainer.scrollHeight +"px";
     }
 });
+
+const modalToTask = () => {
+    let title = document.querySelector("#task").value;
+    let dueDate = document.querySelector("#dueDate").value;
+    let projectName = document.querySelector("#project").value;
+    let taskDone = false;
+    return taskFactory(title, dueDate, projectName, taskDone);
+}
