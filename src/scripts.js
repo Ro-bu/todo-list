63c5ffae32af209044ca50e08a7285b0681513e4
Ro-bu <robin.raid1@gmail.com>
2022-05-04 11:46:09 +0300

    add dummy content if nothing in storage

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,8 @@
 import {modalListeners} from "./modal-controls.js";
 import {addAllProjectsToUi, uiListeners, addAllTasksToUi} from "./ui.js";
+import {Storage} from "./storage.js";
 
-
+Storage.dummyCheck();
 modalListeners();
 addAllTasksToUi();
 addAllProjectsToUi();
--- src/storage.js
+++ src/storage.js
@@ -5,7 +5,78 @@ class Storage {
     static getData() {
         const projectList = JSON.parse(localStorage.getItem("projectList"));
         return projectList;
-    }
+    };
+    static dummyCheck() {
+        if (JSON.parse(localStorage.getItem("projectList")) === null) {
+            localStorage.setItem("projectList", JSON.stringify(dummyContent));
+        };
+    };
 };
+
+let dummyContent = [
+    {
+      "name": "Gym",
+      "color": "#ef2929",
+      "tasks": [
+        {
+          "name": "Max day",
+          "date": "2022-05-06",
+          "done": false
+        },
+        {
+          "name": "Squat 180kg",
+          "date": "",
+          "done": false
+        }
+      ]
+    },
+    {
+      "name": "Work",
+      "color": "#fce94f",
+      "tasks": [
+        {
+          "name": "Team meeting",
+          "date": "2022-05-19",
+          "done": false
+        },
+        {
+          "name": "New project presentation",
+          "date": "2022-05-31",
+          "done": false
+        },
+        {
+          "name": "Madeleine's bday",
+          "date": "2022-08-17",
+          "done": false
+        },
+        {
+          "name": "Get cookies for the team",
+          "date": "2022-04-06",
+          "done": true
+        }
+      ]
+    },
+    {
+      "name": "Chores",
+      "color": "#204a87",
+      "tasks": [
+        {
+          "name": "Wash the car",
+          "date": "",
+          "done": false
+        },
+        {
+          "name": "Batteries for the remote",
+          "date": "",
+          "done": true
+        },
+        {
+          "name": "New lightbulb for the bathroom",
+          "date": "",
+          "done": true
+        }
+      ]
+    }
+  ];
 
 export {Storage};
